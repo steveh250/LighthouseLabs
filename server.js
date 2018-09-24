e2ceeed6b10e2f92b2c49935f64b69fdef94b0c7
Steve <steve.harris@shaw.ca>
2018-09-23 19:32:19 -0700

    Moved node configuration from server.js.txt to server.js and removed the old server.js.txt file.

--- server.js
+++ server.js
@@ -1,1 +1,11 @@
-console.log('hello from our node script!');
+var express = require('express');
+var app = express();
+
+var http = require('http');
+var server = http.Server(app);
+
+app.use(express.static('client'));
+
+server.listen(8080, function() {
+  console.log('Chat server running');
+});
