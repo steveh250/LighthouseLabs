c59cccc607aedf1352755e698aa185c5e17253d9
Steve <steve.harris@shaw.ca>
2018-09-24 20:17:51 -0700

    Added functionality to display message array to newly attached clients

--- client/app.js
+++ client/app.js
@@ -6,7 +6,7 @@ $('form').submit(function ()
   var InitText = $('#initials').val();
   var MsgText = $('#message').val();
 
-  /* Send the message */
+  /* Send the message in parts*/
   socket.emit('InitMsg', InitText, MsgText);
 
   /* Clear out the variables */
@@ -16,13 +16,10 @@ $('form').submit(function ()
 });
 
 /* RECEIVE - This section receives a message from the server and displays it on the web page - hence the <li> */
-socket.on('InitMsg', function (InitText, MsgText) 
+/* Message is recieved back as a single string to leave the parts in the server in case they need processing */
+/* Don't need to receive the message in parts as it's only for display */
+socket.on('InitMsg', function (Message) 
 {
-  /* Lets build the message */
-  var Message = InitText;
-  Message += " says: ";
-  Message += MsgText;
-
-  /* Lets write the line to the screen */
+    /* Lets write the line to the screen */
   $('<li>').text(Message).appendTo('#history');
 });
