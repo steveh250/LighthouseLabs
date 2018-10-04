17c3fd8c9df4cedc09a83dd59435bd92934b9c0e
Steve <steve.harris@shaw.ca>
2018-10-04 06:16:03 -0700

    Day 4 - totalCells

---
+++ Day 4/app-Day4.js
@@ -0,0 +1,40 @@
+const GRID = [
+      ["", "", "", "^", "", "", "", "", "", ""],
+      ["", "", "v", "", "~", "", "", "", "", ""],
+      ["", "v", "", "", "^", "^", "", "", "", ""],
+      ["", "", "", "", "^", "^", "", "", "", ""],
+      ["", "", "", "", "", "", "", "", "v", ""],
+      ["", "", "", "", "", "", "", "", "", ""],
+      ["", "", "", "", "", "", "", "", "", ""],
+      ["", "^", "~", "~", "", "", "", "^", "", ""],
+      ["", "^", "", "~", "~", "", "", "", "", ""],
+      ["", "^", "", "", "~", "~", "", "", "", ""],
+    ];
+
+/* Function to grid */
+function totalCells() {
+  /* Based loosely on https://www.w3resource.com/jquery-exercises/part1/jquery-practical-exercise-23.php */
+  
+  /*********************************************/
+  /* Function to return the cols */
+  function countColumns() {
+    return GRID[0].length;
+  }
+  
+  /* Define variable to hold number of cols and count the cols*/
+  var colctr = countColumns();
+  
+  /*********************************************/
+  /* Function to return the rows */
+  function countRows() {
+    return GRID.length;
+  }
+  
+  /* Define variable to hold number of rows and count the rows*/
+  var rowctr = countRows();
+
+  /* Output cols to console */
+  console.log(colctr*rowctr);
+}
+
+totalCells();
