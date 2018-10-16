34d4f5c39064a6b0ceab4c83552fe5096174918c
Steve <steve.harris@shaw.ca>
2018-10-16 06:07:29 -0700

    Day 15 - firstRock - used Day 13 allRocks code and just return first array element.

---
+++ Day 15/app-Day15.js
@@ -0,0 +1,61 @@
+const GRID = [
+      ["", "", "", "^", "", "", "", "", "", ""],
+      ["", "", "v", "", "~", "", "", "", "", ""],
+      ["", "v", "", "", "^", "^", "", "", "", ""],
+      ["", "", "", "", "^", "^", "", "", "", ""],
+      ["", "", "", "", "", "", "", "", "v", ""],
+      ["", "", "", "", "", "", "", "", "", ""],
+      ["", "", "", "", "", "", "", "", "", ""],
+      ["", "^", "~", "~", "", "", "", "^", "", ""],
+      ["", "^", "", "~", "~", "", "", "", "", ""],
+      ["", "^", "", "", "~", "~", "", "", "", ""],
+    ];
+
+/* Function to return the rows */
+function rowCount() {
+  return GRID.length;
+}
+
+
+function allRocks(){
+
+	/* Array to store the rocks - rockArray */
+	var rockArray = [];
+
+	/* The length of the array */
+	var noOfRows =rowCount();
+	var outsideCount=0;
+
+	/* For all rows in GRID -the array */
+	for (outsideCount=0; outsideCount<noOfRows; outsideCount++) {
+
+		/* For all elements in columns of the array row we are working on - allow for uneven columns if need be*/
+		var insideCount=GRID[outsideCount].length;
+
+		/*Loop through every element of this row of the array */
+		for (i=0; i<insideCount; i++) {
+			if (GRID[outsideCount][i] == "^"){
+				/* We found a rock */
+				/* Row is a number so add 1*/
+				var foundRow=outsideCount+1;
+				
+				/* Column is a letter so index needs to be converted*/
+				var foundColumn = String.fromCharCode(i + 65);
+				
+				/* Add the location to the end of the array */
+				rockArray.push(foundColumn.concat(foundRow));
+			}
+		}
+	}
+
+	/* All rows are processed  lets return the array */
+	return(rockArray);
+}
+
+function firstRock(){
+	/* Populate an array with all the rocks*/
+	var firstRockArray=allRocks();
+
+	/*Return the first rock */
+	return(firstRockArray[0]);
+}
