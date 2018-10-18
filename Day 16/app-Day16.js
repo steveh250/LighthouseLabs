656ca40537d43b389d319501ece4e302bae3b8fd
Steve <steve.harris@shaw.ca>
2018-10-17 18:08:56 -0700

    day16 - firstCurrent

---
+++ Day 16/app-Day16.js
@@ -0,0 +1,62 @@
+const GRID = [
+      ["", "", "", "^", "", "", "", "", "", ""],
+      ["", "", "v", "", "~", "", "", "", "", ""],
+      ["", "v", "", "", "^", "^", "", "", "", ""],
+      ["", "", "", "", "^", "^", "", "", "", ""],
+      ["", "", "", "", "", "", "", "", "v", ""],
+      ["", "", "", "", "", "", "", "", "", ""],
+      ["", "", "", "", "", "", "", "", "", ""],
+      ["", "^", "~", "~", "", "", "", "^", "", ""],
+      ["", "^", "", "~", "~", "", "", "", "", ""],
+      ["", "^", "", "", "~", "~", "", "", "", ""],
+    ];
+
+/* Function to return the rows */
+function rowCount() {
+  return GRID.length;
+}
+
+function allCurrents(){
+
+	/* Array to store the currents - currentArray */
+	var currentArray = [];
+
+	/* The length of the array */
+	var noOfRows =rowCount();
+	var outsideCount=0;
+
+	/* For all rows in GRID -the array */
+	for (outsideCount=0; outsideCount<noOfRows; outsideCount++) {
+
+		/* For all elements in columns of the array row we are working on - allow for uneven columns if need be*/
+		var insideCount=GRID[outsideCount].length;
+
+		/*Loop through every element of this row of the array */
+		for (i=0; i<insideCount; i++) {
+			if (GRID[outsideCount][i] == "~"){
+				/* We found a current */
+				/* Row is a number so add 1*/
+				var foundRow=outsideCount+1;
+				
+				/* Column is a letter so index needs to be converted*/
+				var foundColumn = String.fromCharCode(i + 65);
+				
+				/* Add the location to the end of the array */
+				currentArray.push(foundColumn.concat(foundRow));
+			}
+		}
+	}
+
+	/* All rows are processed  lets return the array */
+	return(currentArray);
+}
+
+function firstCurrent(){
+	/* Populate an array with all the rocks*/
+	var firstCurrentArray=allCurrents();
+
+	/*Return the first rock */
+	return(firstCurrentArray[0]);
+}
+
+firstCurrent();
